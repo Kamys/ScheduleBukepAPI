1ca3d07ac63d64523fd97015ee6acd8fafe3a803
Hikita Kamyshenko <hnkntoc@gmail.com>
2017-04-19 12:21:40 +0300

    Added out json

--- ScheduleBukepAPI/Program.cs
+++ ScheduleBukepAPI/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,21 @@ namespace ScheduleBukepAPI
     {
         static void Main(string[] args)
         {
+            WebRequest request = WebRequest.Create(
+              "https://my.bukep.ru:447/api/Schedule/GetFaculties?year=2016&idFilial=1000");
+
+            request.Credentials = CredentialCache.DefaultCredentials;
+            WebResponse response = request.GetResponse();
+            Console.WriteLine("Status = " + ((HttpWebResponse)response).StatusDescription);
+
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+             
+            Console.WriteLine(responseFromServer);
+              
+            reader.Close();
+            response.Close();
         }
     }
 }
