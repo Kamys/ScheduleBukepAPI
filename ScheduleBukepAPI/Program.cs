fed0c60c7963f2b0947d7587b1f7419433255c55
Hikita Kamyshenko <hnkntoc@gmail.com>
2017-04-19 13:53:33 +0300

    fix

--- ScheduleBukepAPI/Program.cs
+++ ScheduleBukepAPI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using ScheduleBukepAPI.apiDTO;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,21 +14,41 @@ namespace ScheduleBukepAPI
     {
         static void Main(string[] args)
         {
+            List<Faculty> Faculties = getAllFaculty();
+            foreach (Faculty f in Faculties)
+            {
+                showFaculty(f);
+            }
+        }
+
+        private static string getJsonFromAPI()
+        {
             WebRequest request = WebRequest.Create(
-              "https://my.bukep.ru:447/api/Schedule/GetFaculties?year=2016&idFilial=1000");
+                          "https://my.bukep.ru:447/api/Schedule/GetFaculties?year=2016&idFilial=1000");
 
             request.Credentials = CredentialCache.DefaultCredentials;
             WebResponse response = request.GetResponse();
-            Console.WriteLine("Status = " + ((HttpWebResponse)response).StatusDescription);
 
             Stream dataStream = response.GetResponseStream();
             StreamReader reader = new StreamReader(dataStream);
-            string responseFromServer = reader.ReadToEnd();
-             
-            Console.WriteLine(responseFromServer);
-              
+            string json = reader.ReadToEnd();
+
             reader.Close();
             response.Close();
+
+            return json;
+        }
+
+        private static List<Faculty> getAllFaculty()
+        {
+            string json = getJsonFromAPI();
+
+            return JsonConvert.DeserializeObject<List<Faculty>>(json);
+        }
+
+        private static void showFaculty(Faculty f)
+        {
+            Console.WriteLine("Faculty\n id = {0}\n name = {1}\n isActive = {2}", f.idFaculty, f.name, f.isActiveSchedule);
         }
     }
 }
---
+++ ScheduleBukepAPI/apiDTO/Faculty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleBukepAPI.apiDTO
+{
+    class Faculty
+    {
+        public string idFaculty { get; set; }
+        public string name { get; set; }
+        public bool isActiveSchedule { get; set; }
+    }
+}
