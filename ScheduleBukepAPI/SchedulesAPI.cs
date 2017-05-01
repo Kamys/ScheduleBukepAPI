e14fb11a5b014ca75c31c0179e6655dc86dc4f7f
Hikita Kamyshenko <hnkntoc@gmail.com>
2017-05-01 18:06:35 +0300

    Moved part logic from ServiceSchedules in Service2.

--- ScheduleBukepAPI/SchedulesAPI.cs
+++ ScheduleBukepAPI/SchedulesAPI.cs
@@ -6,39 +6,7 @@ using ScheduleBukepAPI;
 
 class ServiceSchedules
 {
-    
-    public List<Faculty> getFaculties(string year, string idFilial)
-    {
-        HttpHelper httpHelper = new HttpHelper();
-        var parameters = new Dictionary<string, string>();
-        parameters.Add("year", year);
-        parameters.Add("idFilial", idFilial);
-        string json = httpHelper.executeGet("GetFaculties", parameters);
-        return convertJSONToDTO<Faculty>(json);
-    }
-
-    internal List<Specialty> getSpecialtys(string year, string idFilial, string idFaculty)
-    {
-        HttpHelper httpHelper = new HttpHelper();
-        var parameters = new Dictionary<string, string>();
-        parameters.Add("year", year);
-        parameters.Add("idFilial", idFilial);
-        parameters.Add("idFaculty", idFaculty);
-        string json = httpHelper.executeGet("GetSpecialtys", parameters);
-        return convertJSONToDTO<Specialty>(json);
-    }
-
-    internal List<Courses> getCourses(string year, string idFilial, string idFaculty, string idsSpecialty)
-    {
-        HttpHelper httpHelper = new HttpHelper();
-        var parameters = new Dictionary<string, string>();
-        parameters.Add("year", year);
-        parameters.Add("idFilial", idFilial);
-        parameters.Add("idFaculty", idFaculty);
-
-        string json = httpHelper.executePost("GetCourses", parameters, idsSpecialty);
-        return convertJSONToDTO<Courses>(json);
-    }
+   
 
     internal List<GroupLessonDTO> getGroupLessons(string dateFrom, string dateTo, string idsSheduleGroup)
     {
---
+++ ScheduleBukepAPI/Service2.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using ScheduleBukepAPI.apiDTO;
+using System.Collections.Generic;
+
+namespace ScheduleBukepAPI
+{
+    class Service2
+    {
+        public List<Faculty> getFaculties(string year, string idFilial)
+        {
+            HttpHelper httpHelper = new HttpHelper();
+            var parameters = new Dictionary<string, string>();
+            parameters.Add("year", year);
+            parameters.Add("idFilial", idFilial);
+            string json = httpHelper.executeGet("GetFaculties", parameters);
+            return convertJSONToDTO<Faculty>(json);
+        }
+
+        internal List<Specialty> getSpecialtys(string year, string idFilial, string idFaculty)
+        {
+            HttpHelper httpHelper = new HttpHelper();
+            var parameters = new Dictionary<string, string>();
+            parameters.Add("year", year);
+            parameters.Add("idFilial", idFilial);
+            parameters.Add("idFaculty", idFaculty);
+            string json = httpHelper.executeGet("GetSpecialtys", parameters);
+            return convertJSONToDTO<Specialty>(json);
+        }
+
+        internal List<Courses> getCourses(string year, string idFilial, string idFaculty, string idsSpecialty)
+        {
+            HttpHelper httpHelper = new HttpHelper();
+            var parameters = new Dictionary<string, string>();
+            parameters.Add("year", year);
+            parameters.Add("idFilial", idFilial);
+            parameters.Add("idFaculty", idFaculty);
+
+            string json = httpHelper.executePost("GetCourses", parameters, idsSpecialty);
+            return convertJSONToDTO<Courses>(json);
+        }
+
+        //TODO: extract in DTOFactory.
+        private static List<T> convertJSONToDTO<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<List<T>>(json);
+        }
+    }
+
+}
